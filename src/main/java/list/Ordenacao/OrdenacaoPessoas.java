5190c27bf5a7d1b81e82d871db2ebb35618151c9
Caio-Pfaltzgraff <caio.rllcr@gmail.com>
2023-08-22 12:48:00 -0300

    Resolvendo desafio OrdenacaoPessoas e OrdenacaoNumeros

---
+++ src/main/java/list/Ordenacao/OrdenacaoPessoas.java
@@ -0,0 +1,29 @@
+package list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoas {
+
+    private List<Pessoa> pessoas;
+
+    public OrdenacaoPessoas() {
+        pessoas = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, Integer idade, Double altura) {
+        pessoas.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoas);
+        if (!pessoas.isEmpty()) {
+            Collections.sort(pessoasPorIdade);
+            return pessoasPorIdade;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+}
